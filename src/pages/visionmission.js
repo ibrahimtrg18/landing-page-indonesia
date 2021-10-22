0863dbfc133d64a178133db6de49d4e5856bc18e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-22 17:42:54 +0700

    add pages/vissionmission: create new pages for vissionmission add content add style improving

---
+++ src/pages/visionmission.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Menu from "../components/Menu";
+import CardImage from "../components/CardImage";
+import { VisionMissionContainer } from "../styles/VisionMission";
+
+export default function VisionMission() {
+  return (
+    <>
+      <VisionMissionContainer>
+        <h1 className="title text-primary">Vision Mission</h1>
+        <div className="wrapper-paragraph--space vision">
+          <h5 className="text-primary">Our Vision</h5>
+          <p>
+            Becoming number one preferred digital platform of international
+            trades for Indonesia and the world.
+          </p>
+        </div>
+        <div className="wrapper-paragraph--space mission">
+          <h5 className="text-primary">Our Mission</h5>
+          <p>
+            Empowering Indonesia’s MSME nationwide to go international,
+            collaboration with all supporting function such as financial, supply
+            chain and logistic, and expanding international network by building
+            partnership with overseas well known digital platform.
+          </p>
+        </div>
+        <div className="wrapper-paragraph--space values">
+          <h5 className="text-primary">Values</h5>
+          <ul>
+            <li>1. Simple</li>
+            <li>2. Trustworthy</li>
+            <li>3. Problem Solver</li>
+          </ul>
+        </div>
+        <div className="map-indonesia">
+          <Image
+            src="/images/indonesia.png"
+            alt="indonesia map"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <CardImage
+          copyrightText="Copyright 2021 62Trade.com PT. Enam Dua Niaga"
+          src="/images/visionmission.png"
+          alt="about image"
+        />
+      </VisionMissionContainer>
+      <Menu />
+    </>
+  );
+}
---
+++ src/styles/VisionMission.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const VisionMissionContainer = styled.div`
+  padding: 20px;
+
+  & > .title {
+    margin-bottom: 32px;
+  }
+
+  & > .map-indonesia {
+    position: relative;
+    width: 100%;
+    height: 100px;
+    margin-bottom: 30px;
+  }
+`;
