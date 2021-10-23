c5789ccd90becea2c600f2040ddff543e146194d
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-23 17:52:11 +0700

    update components/Navbar: add feature fixed when scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,21 @@
+import React, { useState, useEffect } from "react";
 import { AppBar } from "./styles";
 import Vector from "../../assets/icons/Vector.svg";
 
 export default function Navbar() {
+  const [positionScroll, setPositionScroll] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener("scroll", onListenScroll);
+    return () => document.removeEventListener("scroll", onListenScroll);
+  }, []);
+
+  const onListenScroll = () => {
+    setPositionScroll(window.pageYOffset);
+  };
+
   return (
-    <AppBar>
+    <AppBar transparent={positionScroll === 0}>
       <div className="nav__logo">
         <Vector />
         <div className="nav__logo-text">Logo here</div>
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,13 +1,25 @@
 import styled from "styled-components";
 
 export const AppBar = styled.nav`
+  z-index: 10;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
 
   padding: 20px 16px;
   font-weight: bold;
   color: ${({ theme }) => theme.colors.primary};
+  transition: all 0.2s ease-in-out;
+
+  ${({ transparent, theme }) =>
+    !transparent &&
+    `
+      background-color: ${theme.colors.white};
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+      position: fixed;
+      top: 0;
+    `};
 
   & .nav__logo {
     display: flex;
